01943edea66362cc7ee4ac60aba9da5c51764ad5
Beeraka Krupa Kiranmai <krupakiranmai1122@gmail.com>
2021-11-20 23:36:56 +0530

    sessions used

--- Assignment 3/PART B/app.py
+++ Assignment 3/PART B/app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 from sqlalchemy.orm import session
 from flask import session as sess
-from flask_login import LoginManager, login_manager, login_user, logout_user, login_required,current_user, UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/restaurant'
@@ -13,15 +12,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db = SQLAlchemy(app)
-# sess['loggedIn'] = None
-# sess['type'] = None
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_message = "you are not logged in"
 
 
-class User(UserMixin,db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     type = db.Column(db.String(120))
     username = db.Column(db.String(80))
@@ -31,27 +24,13 @@ class User(UserMixin,db.Model):
         self.type = type
         self.username = username
         self.password = password
-    
-    def is_authenticated(self):
-        return True
 
-    def is_active(self):   
-        return True           
-
-    def is_anonymous(self):
-        return False          
-
-    def get_id(self):         
-        return str(self.id)
     # def __repr__(self):
     #     return '<User %r>' % self.username
     
     def getType(self):
         return  self.type
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 class Menu(db.Model):
 
@@ -77,6 +56,14 @@ class Menu(db.Model):
         item.append(self.half_plate)
         item.append(self.full_plate)
         return item
+    
+
+
+
+
+
+
+
     
 
 # db.drop_all()
@@ -114,30 +101,25 @@ def login():
         if(check_user is None):
             return "id not found"
         else:
-            login_user(check_user)
-            # sess['loggedIn'] = True
-            # sess['type'] = check_user.getType()
-            # print(sess.get("loggedIn"))
+            sess['loggedIn'] = True
+            sess['type'] = check_user.getType()
             return "loggedin successfully"
 
 @app.route("/logout", methods=['GET'])
 def logout():
      if(request.method == 'GET'):
-        # sess.pop('type',None) 
-        # sess.pop('loggedIn',None) 
-        logout_user()
+        sess.pop('type',None) 
+        sess.pop('loggedIn',None) 
         return "logged out successfully"
 
 @app.route("/additem", methods=['POST'])
 def additem():
 
-    # if not current_user.is_authenticated:
-    #     return current_app.login_manager.unauthorized()
-    # if not sess.get("loggedIn"):
-    #     return "you are not loggedin"
+    if not sess.get("loggedIn"):
+        return "you are not loggedin"
     
-    # if sess.get("type") != "chef":
-    #     return "you are allowed to perform this action"
+    if sess.get("type") != "chef":
+        return "you are allowed to perform this action"
     
     if(request.method == 'POST'):
 
@@ -155,11 +137,8 @@ def additem():
 @app.route("/getMenu", methods=['GET'])
 def display_menu():
 
-    # if not current_user.is_authenticated:
-    #     return login_manager.unauthorized()
-    # print(sess.get("loggedIn"))
-    # if not sess.get("loggedIn"):
-    #     return "you are not loggedin"
+    if not sess.get("loggedIn"):
+        return "you are not loggedin"
 
     menu_card = {}
     menu_objects = Menu.query.all()
